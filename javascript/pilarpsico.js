6ef07996022b48c456004c298f44388d610d47dc
pilarpsico <pillastraead@gmail.com>
2024-05-27 16:32:23 -0300

    sobremim

---
+++ javascript/pilarpsico.js
@@ -0,0 +1,16 @@
+
+function trocartexto() {
+    const pilarpsico = document.querySelector(".pilar");
+
+    pilarpsico.innerHTML = "Meus amigos me chamam de pill";    
+};
+
+function voltartexto() {
+    const voltart = document.querySelector(".pilar");
+
+    voltart.innerHTML = "pilarpsico é meu mais novo username do momento.";
+};
+
+
+// colocar esta função depois da classe 
+// onmouseover="trocartexto()" onmouseout="voltartexto()" 
